499a926e02b808746097a9e64702af662e9b1b43
TheMartynasXS <Marcius922@gmail.com>
2024-12-02 10:00:39 +0200

    change

--- 2024/day02/part1.js
+++ 2024/day02/part1.js
@@ -1,14 +1,18 @@
 import input from "../../utils/input.js";
-let lines = input("2024/day02/data.txt").map(line => line.split(" ").map(Number));
+let lines = input("2024/day02/data.txt").map((line) =>
+  line.split(" ").map(Number)
+);
 
-let safe = lines.filter((row) =>{
+let safe = lines.filter((row) => {
   let increasing = row[0] < row[1] ? true : false;
-  return row.reduce((safe, value, index) => safe && (index == 0 || (row[index - 1] < value == increasing)), true)
-})
-.filter((row)=>{
   return row.reduce((safe, value, index) => {
     let size = Math.abs(value - row[index - 1]);
-    return safe && (index == 0 || (size >= 1 && size <= 3));
-  } , true)
-}).length
+    return (
+      safe &&
+      (index == 0 ||
+        row[index - 1] < value == increasing &&
+        (size >= 1 && size <= 3))
+    );
+  }, true);
+}).length;
 console.log(safe);
