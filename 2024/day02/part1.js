8cb702670ced2158205ce5a1a5850eff8305d929
TheMartynasXS <Marcius922@gmail.com>
2024-12-02 10:09:10 +0200

    Part2

--- 2024/day02/part1.js
+++ 2024/day02/part1.js
@@ -3,16 +3,17 @@ let lines = input("2024/day02/data.txt").map((line) =>
   line.split(" ").map(Number)
 );
 
-let safe = lines.filter((row) => {
+let safe = lines.filter((row) => test(row)).length;
+
+function test(row) {
   let increasing = row[0] < row[1] ? true : false;
   return row.reduce((safe, value, index) => {
     let size = Math.abs(value - row[index - 1]);
     return (
       safe &&
       (index == 0 ||
-        row[index - 1] < value == increasing &&
-        (size >= 1 && size <= 3))
+        (row[index - 1] < value == increasing && size >= 1 && size <= 3))
     );
   }, true);
-}).length;
+}
 console.log(safe);
--- 2024/day02/part2.js
+++ 2024/day02/part2.js
@@ -1,2 +1,29 @@
 import input from "../../utils/input.js";
-let lines = input("2024/day02/data.txt");
+let lines = input("2024/day02/data.txt").map((line) =>
+  line.split(" ").map(Number)
+);
+
+let safe = lines.filter((row) => {
+  if (test(row, row.length)) return true;
+  for (let i = 0; i < row.length; i++) {
+    if (test(row.slice(0, i).concat(row.slice(i + 1)), row.length - 1))
+      return true;
+  }
+}).length;
+
+function test(row, comparator) {
+  let increasing = row[0] < row[1] ? true : false;
+  return (
+    row.reduce((safe, value, index) => {
+      let size = Math.abs(value - row[index - 1]);
+      return (
+        safe +
+        (index == 0 ||
+        (row[index - 1] < value == increasing && size >= 1 && size <= 3)
+          ? 1
+          : 0)
+      );
+    }, 0) >= comparator
+  );
+}
+console.log(safe);
