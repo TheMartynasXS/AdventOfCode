adb896be21d81d147e6f6a41c3bd92373c71f824
TheMartynasXS <Marcius922@gmail.com>
2024-12-02 10:39:24 +0200

    fix

--- utils/input.js
+++ utils/input.js
@@ -2,6 +2,6 @@ import { dirname, join } from "path";
 import fs from "fs";
 
 export default function () {
-  path = join(process.cwd(), "data.txt");
+  let path = join(process.cwd(), "data.txt");
   return fs.readFileSync(path).toString().trim().split(/\r\n/);
 }
